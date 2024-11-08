1bb708923687d819128996f08986c8db16f81496
ash <wangyixiao752@163.com>
2024-11-08 10:32:32 +0800

    增加文件访问测试接口

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+
+import os
+import imghdr
+from PIL import Image
+import base64
+import io
 
 app = FastAPI()
 
@@ -11,3 +17,23 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.post("/img_to_base64")
+async def image_to_base64(body: dict = Body(...)):
+    path: str = body.get('path')
+    if path and os.path.exists(path) and os.path.isfile(path):
+        _, file_extension = os.path.splitext(path)
+        image_extensions = {'.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp'}
+        if file_extension.lower() not in image_extensions:
+            raise Exception('not a image file')
+        with Image.open(path) as image:
+            # 创建一个字节流
+            buffered = io.BytesIO()
+            # 将图片保存到字节流
+            image.save(buffered, format='PNG')  # 可以根据需要更改格式
+            # 获取字节流的内容并编码为 Base64
+            img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+        return img_str
+    else:
+        raise Exception('not a valid file path')
